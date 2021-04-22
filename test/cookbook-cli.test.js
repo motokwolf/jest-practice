436e6e43bca1739210a0c4e35997d1caa253b8f4
Karen Carnaval <motok@MotokBook.localdomain>
2021-04-21 23:51:47 -0400

    first cli test

--- test/cookbook-cli.test.js
+++ test/cookbook-cli.test.js
@@ -4,26 +4,45 @@ const { CookbookCli } = require('../src/cookbook-cli');
 describe('CookbookCli', () => {
   describe('Adding recipes', () => {
     test('should accept the recipe information and display the correct message', () => {
-	expect(true).toBe(true);
+	//Arrange
+	const myCookbook = new Cookbook();
+	const myCookbookCli = new CookbookCli(myCookbook);
 
+	//Act
+	const message = myCookbookCli.run('add','P&B Sandwich', ['bread','peanut butter','jam']);
+	
+	//Assert
+	expect(message).toBe('Successfully added the following recipe: P&B Sandwich');
     });
   });
 
   describe('Listing recipes', () => {
     test('should display the correct message listing all of the recipe names', () => {
-
+	//Arrange
+	
+	//Act
+	
+	//Assert
     });
   });
 
   describe('Retrieving a recipe', () => {
     test('should display the ingredients required to make the specified recipe', () => {
-
+	//Arrange
+	
+	//Act
+	
+	//Assert
     });
   });
 
   describe('Deleting a recipe', () => {
     test('should accept the recipe name and display the correct message', () => {
-
+	//Arrange
+	
+	//Act
+	
+	//Assert
     });
   });
 });
